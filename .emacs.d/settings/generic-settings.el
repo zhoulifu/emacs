b87610b7ff926d9a12d5b46a7b744d19bc0afb80
zhoulifu <zlf369777684@gmail.com>
2016-12-09 17:48:14 +0800

    y-or-n-p and paren mode

--- .emacs.d/settings/generic-settings.el
+++ .emacs.d/settings/generic-settings.el
@@ -1,4 +1,8 @@
 (add-hook 'before-save-hook
  (lambda () (if (not (eq major-mode 'markdown-mode)) (delete-trailing-whitespace))))
 
+(show-paren-mode t)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
 (provide 'generic-settings)
